d89d39941175a42d2d8579577c57de3e9749265c
Eric Milliman <emilliman4@gmail.com>
2015-09-23 12:10:34 -0400

    added network plots

--- eda_ejm.R
+++ eda_ejm.R
@@ -3,6 +3,10 @@ setwd("C:/Users/millimanej/Desktop/JHU_DaSH/Alzheimers_Dash/")
 library(slam)
 library(gplots)
 library(RColorBrewer)
+require(ape)
+library(igraph)
+require(reshape2)
+require(reshape)
 load("biogrid2.Rdata")
 
 A<-biogrid2[,c(1,2)] 
@@ -49,6 +53,7 @@ reshape<-reshape[,-1]
 reshape[is.na(reshape)]<-1
 reshape.transform<--log10(reshape)
 reshape.transform[reshape.transform==Inf]<-300
+hist(as.vector(as.matrix(reshape.transform)), breaks=1000)
 
 load("phenotype_by_phenotype_fishers_oddsratio.Rda")
 hist(log2(as.vector(reshape2[,-1])[!is.na(as.vector(reshape[,-1]))]), breaks=100)
@@ -59,14 +64,22 @@ rownames(reshape)<-reshape$Var1
 reshape<-reshape[,-1]
 
 png("Fishers_heatmap.png", height = 6000, width = 12000, units="px")
-heatmap.2(as.matrix(reshape), trace="none",
-          dendrogram ="none", Rowv = F, Colv = F, labRow = "", labCol="")
+heatmap.2(as.matrix(reshape.transform), trace="none",
+          dendrogram ="none", Rowv = T, Colv = F, labRow = "", labCol="")
 dev.off()
 
-png("Fishers_heatmap2.png", height = 6000, width = 12000, units="px")
+hm<-as.matrix(reshape.transform>5)
+mode(hm)<-"numeric"
+
+png("Fishers_heatmap3.png", height = 6000, width = 12000, units="px")
 par(cex=2.5)
-heatmap.2(as.matrix(reshape.transform), trace="none",
-          dendrogram ="none", breaks=seq(0,100,by=25), Rowv = F, Colv = F, labRow = "", labCol="", col=colorRampPalette(c("white","red"))((n=4)))
+heatmap.2(hm, trace="none",
+          dendrogram ="none",breaks=c(0,1,2),
+          Rowv = F, Colv = F, labRow = "", labCol="")
+dev.off()
+
+png("Fishers_heatmap_img.png", height=1500, width=4000, units="px")
+image(hm, col=c("white","black"))
 dev.off()
 
 png("Fishers_test_distribution.png", height = 800, width = 1200, units="px")
@@ -78,3 +91,38 @@ png("Fishers_pvalue_QQ.png", height=800, width = 1200, units="px")
 qqnorm(as.vector(as.matrix(reshape)), pch=19)
 dev.off()
 
+load("../fishertable.tree.Rda")
+plot(cut(ft.tree, h=750)$upper)
+
+
+ft.dendro<-as.dendrogram(ft.tree)
+png("Mouse_Phenoype_hclust_withCut.png", height=800, width = 1600, units="px")
+par(cex=2)
+plot(cut(ft.dendro, h=300)$upper, main="Clustering of Mouse Phenotype")
+dev.off()
+
+png("Mouse_Phenoype_hclust.png", height=800, width = 1600, units="px")
+par(cex=2)
+plot(as.dendrogram(ft.tree, leaflab="none"), main="Clustering of Mouse Phenotype")
+dev.off()
+
+
+net<-graph.incidence(hm[rowSums(hm)>100,], add.names = F)
+png("test_net.png", height=1600, width=1600, units="px")
+plot.igraph(net,layout=layout.fruchterman.reingold,vertex.label.cex=0.2, vertex.size=as.numeric(cut(degree(net, V(net)),5))+2, edge.weight=2)
+dev.off()
+
+net1<-melt(hm)
+table(net1$value)
+head(net1)
+net1<-net1[net1$value>0,]
+
+write.graph(net,"edlist.txt", format="edgelist")
+edges<-read.table("edlist.txt")
+colnames(edges)<-c("from","to")
+colnames(nodes)<-"idn"
+nodes$group<-as.factor("phenotype")
+nodes<-data.frame("idn"=unique(c(edges$from, edges$to)))
+
+forceNetwork(Links=edges, Nodes = nodes, NodeID = "idn", 
+             Group = "group",Source = "from",Target="to")
